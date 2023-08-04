eff8567292c7e51aed2f7d5cde0bb85007859965
SamSong-OSC <sam.song@osckorea.com>
2023-08-04 18:27:44 +0900

    breadcrumbs Router에 적용

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -4,21 +4,5 @@
   </div>
 </template>
 <script setup>
-import { onMounted } from 'vue';
-import { useRoute } from 'vue-router'
-import { useBreadCrumbsStore } from '@/stores/breadCrumbsStore';
 
-const route = useRoute();
-const breadCrumbsStore = useBreadCrumbsStore();
-onMounted(() => {
-  breadCrumbsStore.clearBreadCrumbs();
-  breadCrumbsStore.addBreadCrumb(
-    {
-      title: route.meta.breadcrumb,
-      to: route.path,
-      disabled: true
-    }
-  );
-}
-);
 </script>
